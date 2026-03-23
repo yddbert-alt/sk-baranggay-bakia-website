ada6c38335876dcd1b0849bdcc594a566489d14f
yddbert-alt <yddbert@gmail.com>
2026-03-23 13:44:04 +0800

    Add Flask configuration settings to config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    DEBUG = True
+    TESTING = False
+    SECRET_KEY = 'your_secret_key'
+    DATABASE_URI = 'sqlite:///your_database.db'
+    # Add any other configuration settings you need
